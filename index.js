06ea0313babe4767a3a6214d447b15059365923c
Imran Qasim <imran_qasim@hotmail.com>
2018-05-29 09:40:23 +0500

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.static('public'));
 app.use(cors());
 //const server = app.listen(4000, ()=> console.log("Listening at http://localhost:4000"));
-const server app.listen(process.env.PORT || 3000, function(){
+const server = app.listen(process.env.PORT || 3000, function(){
   console.log('listening on', http.address().port);
 });
 const io = socket(server);
